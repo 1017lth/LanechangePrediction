d3abe264ebd16d4a308095e31c0b0c474fc16457
LeeTaeHo <55744923+1017lth@users.noreply.github.com>
2019-11-04 16:20:19 +0900

    Add files via upload

---
+++ Confusion_matrix(2).py
@@ -0,0 +1,34 @@
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils.multiclass import unique_labels
+import numpy as np
+# import some data to play with
+filename_LCP = "dataset11.csv"
+df_LCP = pd.read_csv(filename_LCP)
+df1 = pd.DataFrame(df_LCP)
+X = df1.drop('Lanemarker',axis=1)
+y = df1['Lanemarker'].astype(np.int64)
+class_names = unique_labels(df1['Lanemarker'])
+
+# Split the data into a training set and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# Run classifier
+classifier = svm.SVC(kernel='linear')
+y_pred = classifier.fit(X_train, y_train).predict(X_test)
+
+# Compute confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+
+print(cm)
+
+# Show confusion matrix in a separate window
+plt.matshow(cm)
+plt.title('Confusion matrix')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
